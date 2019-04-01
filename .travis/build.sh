ae63bc7dab7373c41dfe40486bbcfda572f05733
liushuyu <liushuyu011@gmail.com>
2019-04-01 16:03:52 -0600

    travis: add macos and windows CI

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-cd /liftinstall
+cd /liftinstall || exit 1
 
-apt update
-apt install -y libwebkit2gtk-4.0-dev libssl-dev
+apt-get update
+apt-get install -y libwebkit2gtk-4.0-dev libssl-dev
 
-curl https://sh.rustup.rs -sSf | sh -s -- -y
+curl https://build.travis-ci.org/files/rustup-init.sh -sSf | sh -s -- -y
 export PATH=~/.cargo/bin:$PATH
 
 cargo build
