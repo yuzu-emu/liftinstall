269b083ec82b91325aad96abb09fbe85d6609e98
liushuyu <liushuyu011@gmail.com>
2019-04-03 00:58:49 -0600

    travis: use official Rust Docker image

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,7 +4,4 @@ cd /liftinstall || exit 1
 apt-get update
 apt-get install -y libwebkit2gtk-4.0-dev libssl-dev
 
-curl https://build.travis-ci.org/files/rustup-init.sh -sSf | sh -s -- -y
-export PATH=~/.cargo/bin:$PATH
-
 cargo build
